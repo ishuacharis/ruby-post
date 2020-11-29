4611e6caec7455ce24ebd22f79b9ee1f5a22a2e6
ishuacharis <ishuacharis@gmail.com>
2020-11-29 08:29:08 +0100

    created post controller and its views

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,31 @@
+class PostController < ApplicationController
+
+    # /GET all posts
+    def index
+    end
+    
+    # /GET a form to create a new post
+    def new
+    end
+
+    # /POST create a new post
+    def create
+    end
+
+    # /GET show a single post
+    def show
+    end
+
+    # /GET a form to edit a saved post
+    def edit
+    end
+
+    # /PATCH OR /PUT save an edited post
+    def update
+    end
+
+    # /DELETE delete or destroy a saved post
+    def destroy
+    end
+
+end
