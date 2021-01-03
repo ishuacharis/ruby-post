7ef8c463b168a1024a9b7440e24fa3a5b3856624
ishuacharis <ishuacharis@gmail.com>
2021-01-03 11:18:46 +0100

    added the action mail and gmail configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,8 +31,22 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  # Action mailer config
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_options = {from: 'no-reply@example.com'}
+
+  # Action mailer config for gmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'example.com',
+  user_name:            '<username>',
+  password:             '<password>',
+  authentication:       'plain',
+  enable_starttls_auto: true }
 
   config.action_mailer.perform_caching = false
 
