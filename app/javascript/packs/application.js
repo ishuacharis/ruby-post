019d76baaf1cd85a46e66323a3d72d8d4f7bb439
ishuacharis <ishuacharis@gmail.com>
2021-01-06 04:52:54 +0100

    added stimulus package

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("custom/session")
+require("controllers/session_controller")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -16,3 +16,5 @@ require("custom/session")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import "controllers"
